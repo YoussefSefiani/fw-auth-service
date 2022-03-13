fd9f08b858a5c028001b814c851358adac2ea69e
YoussefSefiani <youssef.sefiani@student.ehb.be>
2022-03-13 20:14:37 +0100

    fixing bugs

--- src/main/java/fw/authservice/service/UserService.java
+++ src/main/java/fw/authservice/service/UserService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.transaction.Transactional;
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
