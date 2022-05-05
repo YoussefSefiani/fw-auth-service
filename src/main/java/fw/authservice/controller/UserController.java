5edc8d409d5da188de9a8b851228e3a87683f5a9
YoussefSefiani <youssef.sefiani@student.ehb.be>
2022-05-05 20:32:21 +0200

    webconfig

--- src/main/java/fw/authservice/controller/UserController.java
+++ src/main/java/fw/authservice/controller/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/user")
-@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
 
     private final UserService userService;
