31cd899fdf9277b7f0ff3fc187ac6655b8ee87ee
YoussefSefiani <youssef.sefiani@student.ehb.be>
2022-05-05 23:37:10 +0200

    webconfig

--- src/main/java/fw/authservice/AuthServiceApplication.java
+++ src/main/java/fw/authservice/AuthServiceApplication.java
@@ -13,6 +13,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={UserDetailsServiceAutoConfiguration.class})
@@ -23,6 +25,20 @@ public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.exposedHeaders("*")
+                		.allowedMethods("*")
+                		.allowedHeaders("*");
+			}
+		};
 	}
 
 }
