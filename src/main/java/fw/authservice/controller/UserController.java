89019208dfc60dce3f69006262c3890b4c0e8ea7
YoussefSefiani <youssef.sefiani@student.ehb.be>
2022-05-05 23:05:17 +0200

    webconfig

--- src/main/java/fw/authservice/controller/UserController.java
+++ src/main/java/fw/authservice/controller/UserController.java
@@ -5,8 +5,6 @@ import fw.authservice.model.User;
 import fw.authservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
