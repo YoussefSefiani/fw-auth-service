57050b438ee7c3d68cfb50e83889293803875665
YoussefSefiani <youssef.sefiani@student.ehb.be>
2022-05-05 20:46:49 +0200

    webconfig

--- src/main/java/fw/authservice/controller/UserController.java
+++ src/main/java/fw/authservice/controller/UserController.java
@@ -31,7 +31,6 @@ public class UserController {
         return userService.getUsers();
     }
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping(path = "{userId}")
     public User getUser(@PathVariable("userId") Long userId) {
         return userService.getUser(userId);
