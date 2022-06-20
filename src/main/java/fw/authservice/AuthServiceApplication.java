d6b411e8bd442980ab6b128a49007216e7c5bc45
YoussefSefiani <youssef.sefiani@student.ehb.be>
2022-06-20 20:13:58 +0200

    host azure

--- src/main/java/fw/authservice/AuthServiceApplication.java
+++ src/main/java/fw/authservice/AuthServiceApplication.java
@@ -15,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableEurekaClient
 @EnableFeignClients
 public class AuthServiceApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
